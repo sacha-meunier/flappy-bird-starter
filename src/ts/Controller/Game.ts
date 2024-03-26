3544f63930f353a5991fd1ea600358112df39ec6
sacha-meunier <meuniersacha@gmail.com>
2024-03-26 14:03:03 +0100

    Using different canvas

--- src/ts/Controller/Game.ts
+++ src/ts/Controller/Game.ts
@@ -1,28 +1,42 @@
 import {IAnimatable} from "../Types/IAnimatable";
 import {Background} from "../Models/Background";
 import {Ground} from "../Models/Ground";
+import {settings} from "../settings";
 
 export class Game {
-    private readonly canvas: HTMLCanvasElement;
-    private readonly ctx: CanvasRenderingContext2D;
+    private readonly backgroundCanvas: HTMLCanvasElement;
+    private readonly backgroundCtx: CanvasRenderingContext2D;
+    private readonly groundCanvas: HTMLCanvasElement;
+    private readonly groundCtx: CanvasRenderingContext2D;
+    private readonly birdieCanvas: HTMLCanvasElement;
+    private readonly birdieCtx: CanvasRenderingContext2D;
+    private readonly pipesCanvas: HTMLCanvasElement;
+    private readonly pipesCtx: CanvasRenderingContext2D;
     private readonly sprite: HTMLImageElement;
     private drawables: IAnimatable[];
 
     constructor() {
-        this.canvas = document.getElementById('game') as HTMLCanvasElement;
-        this.ctx = this.canvas.getContext('2d');
+        this.backgroundCanvas = document.getElementById(settings.background.selector) as HTMLCanvasElement;
+        this.backgroundCtx = this.backgroundCanvas.getContext('2d');
+        this.groundCanvas = document.getElementById(settings.ground.selector) as HTMLCanvasElement;
+        this.groundCtx = this.groundCanvas.getContext('2d');
+        this.birdieCanvas = document.getElementById(settings.birdie.selector) as HTMLCanvasElement;
+        this.birdieCtx = this.birdieCanvas.getContext('2d');
+        this.pipesCanvas = document.getElementById(settings.tubesPair.selector) as HTMLCanvasElement;
+        this.pipesCtx = this.pipesCanvas.getContext('2d');
+
         this.sprite = new Image();
         this.sprite.src = 'src/resources/sprite.png';
 
         this.drawables = [
-            new Background(this.ctx, this.canvas, this.sprite),
-            new Ground(this.ctx, this.canvas, this.sprite),
+            new Background(this.backgroundCtx, this.backgroundCanvas, this.sprite),
+            new Ground(this.groundCtx, this.groundCanvas, this.sprite),
         ];
 
         this.addEventListeners();
     }
 
-    addEventListeners() {
+    private addEventListeners() {
         this.sprite.addEventListener('load', () => {
             this.animate();
         });
